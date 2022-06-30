9ad61cc3f536898efec6a075ebe6d20234f6fb56
Benran Zhang <jimyzhan@usc.edu>
2022-06-29 18:34:13 -0700

    created views to handel https request

--- Sound/urls.py
+++ Sound/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('HelloWorld/', views.index),
+    path('Test/', views.text),
 ]
