c0593e4b6302db777ea3e5c83e6be8ecf2aedc33
Benran Zhang <jimyzhan@usc.edu>
2022-06-29 19:50:18 -0700

    created the item model which models the individual shopable items

--- Sound/models.py
+++ Sound/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+# This is the generic class for an item that can be gutiar, electric, bass or other
+class Item(models.Model):
+    # name of the item
+    name = models.CharField(max_length=100)
+    # -1 = default null instrument: will not be displayed
+    # 0-5 are normal instrument types
+    type = models.IntegerField(default=-1)
+    # price of the item
+    price = models.FloatField(default=0)
+    # color of item
+    color = models.CharField(max_length=100)
+    # top wood type
+    top_wood = models.CharField(max_length=100)
+    # back wood type
+    back_wood = models.CharField(max_length=100)
+    # has electronics?
+    has_electronics = models.BooleanField(default=0)
+    # short description
+    short_description = models.TextField()
+    # number of string
+    num_of_strings = models.IntegerField()
+    # brand
+    brand = models.CharField(max_length=100)
+
+
